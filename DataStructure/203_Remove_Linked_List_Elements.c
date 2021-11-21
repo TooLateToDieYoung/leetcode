f3887335a631a9c0034d45c75bddb3991942fc1a
TooLateToDieYoung <toLateToDieYoung1204@gmail.com>
2021-11-21 17:51:25 +0800

    Create 203_Remove_Linked_List_Elements.c

---
+++ DataStructure/203_Remove_Linked_List_Elements.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+typedef struct ListNode node_t;
+
+/* First Trying Runtime = 12ms *//*
+node_t * removeElements(node_t * head, int val){
+    
+    if(!head) return NULL; // 本來就空
+    
+    while(head && head->val == val) head = head->next; // 前幾個就不要
+    
+    if(!head || !head->next) // 檢查[刪到沒東西]，跟[刪到剩一個]
+        return ( !head || head->val == val ) ? NULL : head;
+        
+    node_t * front = head, * curr = front->next;
+    
+    while(curr){
+        if(curr->val == val) front->next = curr->next;
+        else front = front->next;
+        curr = front->next;
+    }
+    
+    return head;
+}*/
+
+
+/* Second Trying Runtime = 8ms */
+node_t * removeElements(node_t * head, int val){
+    if(!head) return NULL;
+    if(head->val == val) return removeElements(head->next, val);
+    head->next = removeElements(head->next, val);
+    return head;
+}
