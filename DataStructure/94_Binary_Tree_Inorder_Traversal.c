90ef025dcd86e27ad78d3ffb9eeddc6eced5bca3
TooLateToDieYoung <toLateToDieYoung1204@gmail.com>
2021-11-22 09:49:32 +0800

    Create 94_Binary_Tree_Inorder_Traversal.c

---
+++ DataStructure/94_Binary_Tree_Inorder_Traversal.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct TreeNode tree_t;
+
+int TreeSize(const tree_t * self){
+    return (self) ? TreeSize(self->left) + TreeSize(self->right) + 1 : 0;
+}
+
+int * _InorderTraversal(tree_t * self, int * curr){
+    if(self->left) curr = _InorderTraversal(self->left, curr);
+    *curr++ = self->val;
+    if(self->right) curr = _InorderTraversal(self->right, curr);
+    return curr;
+}
+
+int * inorderTraversal(tree_t * root, int * returnSize){
+    if(!root) return (int*)malloc((*returnSize = 0)*sizeof(int));
+    
+    *returnSize = TreeSize(root);
+    int * ans = (int*)malloc((*returnSize)*sizeof(int));
+    int * curr = ans;
+    _InorderTraversal(root, curr);
+    
+    return ans;
+}
