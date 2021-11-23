5586791a49048f48e0389e07239f168f8f1403be
TooLateToDieYoung <toLateToDieYoung1204@gmail.com>
2021-11-23 19:26:06 +0800

    Update 700_Search_in_a_Binary_Search_Tree.c

--- DataStructure/700_Search_in_a_Binary_Search_Tree.c
+++ DataStructure/700_Search_in_a_Binary_Search_Tree.c
@@ -1,6 +1,7 @@
 
 typedef struct TreeNode tree_t;
 
+/* First Trying Runtime = 20ms */
 tree_t * searchBST(const tree_t * root, const int val){
     if(root == NULL) return root;
     if(root->val == val) return root;
@@ -12,4 +13,18 @@ tree_t * searchBST(const tree_t * root, const int val){
     if(temp != NULL) return temp;
     
     return NULL;
+}
+
+/* Second Trying Runtime = 16ms */
+tree_t * searchBST(const tree_t * root, const int val){
+    
+    if(root == NULL) return root;
+    if(root->val == val) return root;
+    tree_t * temp;
+    
+    if(root->val > val) temp = searchBST(root->left, val);
+    else temp = searchBST(root->right, val);
+    
+    if(temp != NULL) return temp;
+    else return NULL;
 }
