e27bb149d1cebda14998ab47dea830c971f6944c
TooLateToDieYoung <toLateToDieYoung1204@gmail.com>
2021-11-23 21:07:27 +0800

    Update 701_Insert_into_a_Binary_Search_Tree.c

--- DataStructure/701_Insert_into_a_Binary_Search_Tree.c
+++ DataStructure/701_Insert_into_a_Binary_Search_Tree.c
@@ -1,31 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
 
 typedef struct TreeNode tree_t;
 
-/* First Trying Runtime = 40ms(win 20%), Memory = 22.6MB(win 93%). */
+// if we want to check for "left path" or "right path", but it's nullptr.
+// Then the current node is parent who being inserted into its rear.
 tree_t * _FindParent(tree_t * self, const int val){
     if(self->val > val && self->left ) return _FindParent(self->left,  val);
     if(self->val < val && self->right) return _FindParent(self->right, val);
-    return self;
+    return self; 
 }
 
 tree_t * insertIntoBST(tree_t * root, int val){
     
+	// Make sure that the first node has a value.
     if(root == NULL){
         root = (tree_t*)calloc(1, sizeof(tree_t));
         root->val = val;
         return root;
     }
     
+	// Looking for the node that new data add in its rear.
     tree_t * parent = _FindParent(root, val);
     tree_t * add = (tree_t*)calloc(1, sizeof(tree_t));
+	
+	// Just assign data and add in trrreeeeeee.
     if(add){
         add->val = val;
         if(parent->val > add->val) parent->left = add;
